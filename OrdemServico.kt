2685b76f7cebf98a0040be37f86238ebd19a28e8
Leonardo Azevedo <83608386+Azevedo1996@users.noreply.github.com>
2025-03-05 15:06:07 -0300

    Create OrdemServico.kt

---
+++ OrdemServico.kt
@@ -0,0 +1,8 @@
+data class OrdemServico(
+    val id: Long,
+    val cliente: String,
+    val descricao: String,
+    val status: String,
+    val dataCriacao: String,
+    val tecnicoResponsavel: String
+)
